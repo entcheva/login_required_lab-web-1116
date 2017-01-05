1b1b1dc2dd9902862646081510643cc53253c568
entcheva <christina.entcheva@gmail.com>
2017-01-04 19:40:14 -0500

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,10 @@
-class SecretsController < ActionController::Base
+class SecretsController < ApplicationController
+  before_action :current_user
 
   def show
+    if !current_user
+      redirect_to login_path
+    end
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,20 @@
-class SessionsController < ActionController::Base
+class SessionsController < ApplicationController
 
   def new
   end
 
   def create
+    unless params[:name].blank?
+      session[:name] = params[:name]
+      redirect_to secrets_path
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy
+    session[:name] = nil
+    redirect_to login_path
   end
 
 
